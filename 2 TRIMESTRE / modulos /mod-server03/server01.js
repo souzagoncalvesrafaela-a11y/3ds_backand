ea60f3fa8fe7ba09d401735b94fbd61068866f7a
souzagoncalvesrafaela-a11y <souza.goncalves.rafaela@escola.pr.gov.br>
2026-08-06 13:30:20 +0000

    criação do servidor HTTP e configuração de rotas

---
+++ 2 TRIMESTRE / modulos /mod-server03/server01.js
@@ -0,0 +1,13 @@
+// Importar móduos
+const http = require('node:http')
+const porta = 8001
+
+// criar servidor e rotas 
+const server = htto.createServer(function (req, res) {} => {
+    // rotas e conteúidos 
+})
+
+// liberar porta no meu PC
+server.listen(porta, () => {
+   console.log (`Servidor rodando em http://localhost:${porta}`
+   })
