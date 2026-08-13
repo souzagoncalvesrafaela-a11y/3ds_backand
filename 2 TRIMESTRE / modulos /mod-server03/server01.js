3bf48abd9941590824efaa44c7a5b5f1580c3632
souzagoncalvesrafaela-a11y <souza.goncalves.rafaela@escola.pr.gov.br>
2026-08-13 13:29:55 +0000

    corrigir erros de digitação e adicionar páginas HTML para navegação

--- 2 TRIMESTRE / modulos /mod-server03/server01.js
+++ 2 TRIMESTRE / modulos /mod-server03/server01.js
@@ -1,13 +1,32 @@
-// Importar móduos
+// Importar módulos
 const http = require('node:http')
+const fs = require('node:fs')
+const path = require('node:path')
 const porta = 8001
 
-// criar servidor e rotas 
-const server = htto.createServer(function (req, res) {} => {
-    // rotas e conteúidos 
+const home = path.join(__dirname, '/pages/index.html')
+const compras = path.join(__dirname, '/pages/comprtas.html')
+
+// criar servidor e rotas
+const server = http.createServer((req, res) => {
+// rotas e conteúdos
+const urlTratada = new URL(req.url, `http://${req.headers.host}`)
+const caminhoPesquisado = urlTratada.pathname
+if (caminhoPesquisado === '/') {
+   res.setHeader('Content-Type', 'text/html; charset=utf8')
+   return res.end(fs.readFileSync(home, 'utf8'))
+}
+if (caminhoPesquisado === '/compras') {
+   res.setHeader('Content-Type', 'text/html; charset=utf8')
+   return res.end(fs.readFileSync(compras, 'utf8'))
+}else {
+res.setHeader('Content-Type', 'text/html; charset=utf8')
+res.end('<h3>Página não permitida</h3>')
+}
+
 })
 
 // liberar porta no meu PC
 server.listen(porta, () => {
-   console.log (`Servidor rodando em http://localhost:${porta}`
-   })
+console.log(`Servidor rodando em http://localhost:${porta}`)
+})
